e483ac1b69a793a3e3698782fcb0d5f2f16abad6
Samay001 <samayrathod1@gmail.com>
2024-07-04 01:17:38 +0530

    started with home page and added about me section

---
+++ src/pages/about.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './style/about.css'
+
+const about = () => {
+  return (
+    <div className='container'>
+      <div className='text-box'>
+        <h1>INDIAN-BASED ART DIRECTOR , DESIGNER AND DEVELOPER.</h1>
+        <h2>TELLING MEANING FULL STORIES THAT NEED TO BE TOLD (BY HUMAN,NOT BY AI)</h2>
+        <h4>WORKING WITH PASSIONATE PEOPLE AND DISRUPTOR TO CREATE MEMORABLE BRANDS AND CAPTIVATION DIGITAL EXPERIENCE THAT DELIVERS RESULTS.</h4>
+      </div>
+    </div>
+  )
+}
+
+export default about
