775b2a8c1876a379d399fedf04fc78b878397f74
Samay001 <samayrathod1@gmail.com>
2024-07-08 14:23:19 +0530

    little changes in form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,6 +3,31 @@ import './style/contact.css';
 import img from '../assets/images/contact-img.jpg';
 
 const Contact = () => {
+  const [result, setResult] = React.useState("");
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "d1b7b85e-1f59-453b-b94e-bfe3a7ca66de");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("Form Submitted Successfully");
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      setResult(data.message);
+    }
+  };
+  
   return (
     <div className='container-contact'>
       <div className='contact-content'>
@@ -11,12 +36,13 @@ const Contact = () => {
           <h1>Let's Do This.</h1>
         </div>
         <div className='contact-form'>
-          <form action='https://formsubmit.co/samayrathod1@gmail.com' method='POST'>
-            <input type='text' placeholder='NAME' className='form-input' required />
-            <input type='email' placeholder='Email' className='form-input' required />
-            <textarea placeholder='HOW CAN I HELP YOU?' className='form-textarea' required></textarea>
+          <form onSubmit={handleSubmit}>
+            <input type='text' name='name' placeholder='NAME' className='form-input' required />
+            <input type='email' name='email' placeholder='EMAIL' className='form-input' required />
+            <textarea name='message' placeholder='HOW CAN I HELP YOU?' className='form-textarea' required></textarea>
             <button type='submit' className='form-button'>SEND</button>
           </form>
+          <span>{result}</span>
         </div>
       </div>
       <div className='contact-img'>
